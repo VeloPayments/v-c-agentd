71bdae40e9b8a0455b84d1eb2aac5ed1bcb3e815
Justin Handville <nanolith@gmail.com>
2022-01-26 02:03:25 -0500

    Code to send context requests to dataservice.

--- src/protocolservice/pde_decode_and_dispatch_req_context_open.c
+++ src/protocolservice/pde_decode_and_dispatch_req_context_open.c
@@ -7,6 +7,7 @@
  * \copyright 2022 Velo Payments, Inc.  All rights reserved.
  */
 
+#include <agentd/dataservice/api.h>
 #include <agentd/randomservice/api.h>
 #include <cbmc/model_assert.h>
 #include <unistd.h>
@@ -14,6 +15,10 @@
 #include "protocolservice_internal.h"
 
 #if defined(AGENTD_NEW_PROTOCOL)
+
+RCPR_IMPORT_allocator_as(rcpr);
+RCPR_IMPORT_rbtree;
+RCPR_IMPORT_resource;
 
 /**
  * \brief Decode and dispatch a dataservice context open request.
@@ -35,10 +40,123 @@ status pde_decode_and_dispatch_req_context_open(
     RCPR_SYM(mailbox_address) return_address,
     protocolservice_dataservice_response_message** reply_payload)
 {
-    /* TODO - implement. For now, just mark it as an invalid request. */
-    return
-        pde_decode_and_dispatch_invalid_req(
-            ctx, req_payload, return_address, reply_payload);
+    status retval, release_retval;
+    uint32_t offset, status, child;
+    protocolservice_dataservice_mailbox_context_entry* tmp = NULL;
+
+    /* parameter sanity checks. */
+    MODEL_ASSERT(prop_protocolservice_dataservice_endpoint_context_valid(ctx));
+    MODEL_ASSERT(
+        prop_protocolservice_dataservice_request_message_valid(req_payload));
+    MODEL_ASSERT(return_address > 0);
+    MODEL_ASSERT(NULL != reply_payload);
+
+    /* create a mailbox_context entry. */
+    retval = rcpr_allocator_allocate(ctx->alloc, (void**)&tmp, sizeof(*tmp));
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* clear entry. */
+    memset(tmp, 0, sizeof(*tmp));
+
+    /* init entry. */
+    resource_init(
+        &tmp->hdr, &protocolservice_dataservice_mailbox_context_release);
+    tmp->alloc = ctx->alloc;
+    tmp->reference_count = 1;
+    tmp->addr = return_address;
+
+    /* send a dataservice child context create request to the data service. */
+    retval =
+        dataservice_api_sendreq_child_context_create(    
+            ctx->datasock, req_payload->payload.data,
+            req_payload->payload.size);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto cleanup_mailbox_context;
+    }
+
+    /* read the response from this operation. */
+    retval =
+        dataservice_api_recvresp_child_context_create(
+            ctx->datasock, ctx->alloc, &offset, &status, &child);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto cleanup_mailbox_context;
+    }
+
+    /* verify that context allocation was successful. */
+    if (STATUS_SUCCESS != status)
+    {
+        goto cleanup_mailbox_context;
+    }
+
+    /* set the context. */
+    tmp->context = child;
+
+    /* insert this record into the mailbox_context map. */
+    retval = rbtree_insert(ctx->mailbox_context_tree, &tmp->hdr);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto cleanup_mailbox_context;
+    }
+
+    /* bump the reference count. */
+    tmp->reference_count += 1;
+
+    /* insert this record into the context_mailbox map. */
+    retval = rbtree_insert(ctx->context_mailbox_tree, &tmp->hdr);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto remove_mailbox_context_entry;
+    }
+
+    /* on success, this map owns our reference. */
+    tmp = NULL;
+
+    /* create the response message. */
+    retval =
+        protocolservice_dataservice_response_message_create(
+            reply_payload, ctx, req_payload->request_id, STATUS_SUCCESS,
+            req_payload->offset, NULL);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto remove_context_mailbox_entry;
+    }
+
+    /* success. */
+    goto done;
+
+remove_context_mailbox_entry:
+    release_retval = rbtree_delete(NULL, ctx->context_mailbox_tree, &child);
+    if (STATUS_SUCCESS != release_retval)
+    {
+        retval = release_retval;
+    }
+
+remove_mailbox_context_entry:
+    release_retval =
+        rbtree_delete(NULL, ctx->mailbox_context_tree, &return_address);
+    if (STATUS_SUCCESS != release_retval)
+    {
+        retval = release_retval;
+    }
+
+cleanup_mailbox_context:
+    if (NULL != tmp)
+    {
+        release_retval = resource_release(&tmp->hdr);
+        if (STATUS_SUCCESS != release_retval)
+        {
+            retval = release_retval;
+        }
+        tmp = NULL;
+    }
+
+done:
+    return retval;
 }
 
 #endif /* defined(AGENTD_NEW_PROTOCOL) */
---
+++ src/protocolservice/protocolservice_dataservice_mailbox_context_release.c
@@ -0,0 +1,55 @@
+/**
+ * \file protocolservice/protocolservice_dataservice_mailbox_context_release.c
+ *
+ * \brief Release a mailbox_context entry.
+ *
+ * \copyright 2022 Velo Payments, Inc.  All rights reserved.
+ */
+
+#include <agentd/randomservice/api.h>
+#include <cbmc/model_assert.h>
+#include <unistd.h>
+
+#include "protocolservice_internal.h"
+
+#if defined(AGENTD_NEW_PROTOCOL)
+
+RCPR_IMPORT_allocator_as(rcpr);
+
+/**
+ * \brief Release a mailbox context resource.
+ *
+ * \param r             The resource to release.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+status protocolservice_dataservice_mailbox_context_release(
+    RCPR_SYM(resource)* r)
+{
+    protocolservice_dataservice_mailbox_context_entry* entry =
+        (protocolservice_dataservice_mailbox_context_entry*)r;
+
+    /* parameter sanity checks. */
+    MODEL_ASSERT(
+        prop_protocolservice_dataservice_mailbox_context_entry_valid(entry));
+
+    /* decrement the reference count. */
+    entry->reference_count -= 1;
+
+    /* if the reference count is still positive, exit. */
+    if (entry->reference_count > 0)
+    {
+        return STATUS_SUCCESS;
+    }
+
+    /* cache allocator. */
+    rcpr_allocator* alloc = entry->alloc;
+
+    /* reclaim memory. */
+    return
+        rcpr_allocator_reclaim(alloc, entry);
+}
+
+#endif /* defined(AGENTD_NEW_PROTOCOL) */
