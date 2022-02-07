2a4c0b52c85d5a713d3ee90fe0f0a1cf30246894
Justin Handville <nanolith@gmail.com>
2022-02-07 16:12:06 -0500

    Added encoder for transaction_get_first.

--- test/dataservice/test_dataservice_encode.cpp
+++ test/dataservice/test_dataservice_encode.cpp
@@ -1241,3 +1241,79 @@ TEST(dataservice_encode_test, request_transaction_get_decoded)
     dispose((disposable_t*)&req);
     dispose((disposable_t*)&alloc_opts);
 }
+
+/**
+ * Test that the encode function performs parameter checks.
+ */
+TEST(dataservice_encode_test, request_transaction_get_first)
+{
+    allocator_options_t alloc_opts;
+    vccrypt_buffer_t buffer;
+    const uint32_t child = 0x1234;
+
+    malloc_allocator_options_init(&alloc_opts);
+
+    /* a NULL buffer is invalid. */
+    EXPECT_EQ(AGENTD_ERROR_DATASERVICE_INVALID_PARAMETER,
+        dataservice_encode_request_transaction_get_first(
+            nullptr, &alloc_opts, child));
+
+    /* a NULL allocator is invalid. */
+    EXPECT_EQ(AGENTD_ERROR_DATASERVICE_INVALID_PARAMETER,
+        dataservice_encode_request_transaction_get_first(
+            &buffer, nullptr, child));
+
+    /* clean up. */
+    dispose((disposable_t*)&alloc_opts);
+}
+
+/**
+ * Test that the decoded values match the encoded values.
+ */
+TEST(dataservice_encode_test, request_transaction_get_first_decoded)
+{
+    allocator_options_t alloc_opts;
+    vccrypt_buffer_t buffer;
+    dataservice_request_transaction_get_first_t req;
+    const uint32_t child = 0x1234;
+
+    malloc_allocator_options_init(&alloc_opts);
+
+    /* the encode call should succeed. */
+    ASSERT_EQ(STATUS_SUCCESS,
+        dataservice_encode_request_transaction_get_first(
+            &buffer, &alloc_opts, child));
+
+    /* make working with the request more convenient. */
+    const uint8_t* breq = (const uint8_t*)buffer.data;
+
+    /* the payload should be at least large enough for the method. */
+    ASSERT_GE(buffer.size, sizeof(uint32_t));
+
+    /* get the method. */
+    uint32_t nmethod = 0U;
+    memcpy(&nmethod, breq, sizeof(uint32_t));
+    uint32_t method = htonl(nmethod);
+
+    /* the method should be DATASERVICE_API_METHOD_APP_PQ_TRANSACTION_READ */
+    ASSERT_EQ(DATASERVICE_API_METHOD_APP_PQ_TRANSACTION_FIRST_READ, method);
+
+    /* increment breq past command. */
+    breq += sizeof(uint32_t);
+
+    /* derive the payload size. */
+    size_t payload_size = buffer.size - sizeof(uint32_t);
+
+    /* the decode should succeed. */
+    ASSERT_EQ(STATUS_SUCCESS,
+        dataservice_decode_request_transaction_get_first(
+            breq, payload_size, &req));
+
+    /* the child index should match. */
+    EXPECT_EQ(child, req.hdr.child_index);
+
+    /* clean up. */
+    dispose((disposable_t*)&buffer);
+    dispose((disposable_t*)&req);
+    dispose((disposable_t*)&alloc_opts);
+}
