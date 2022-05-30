ceef6efa0f5685a3bbd20599fd443cf99156bd86
Justin Handville <nanolith@gmail.com>
2022-05-30 18:29:36 -0400

    Assertion cancel request implementation.

---
+++ src/notificationservice/notificationservice_api_sendreq_assertion_cancel.c
@@ -0,0 +1,79 @@
+/**
+ * \file notificationservice/notificationservice_api_sendreq_assertion_cancel.c
+ *
+ * \brief Send an assertion cancel request.
+ *
+ * \copyright 2022 Velo Payments, Inc.  All rights reserved.
+ */
+
+#include <agentd/bitcap.h>
+#include <agentd/inet.h>
+#include <agentd/notificationservice/api.h>
+#include <agentd/status_codes.h>
+#include <cbmc/model_assert.h>
+
+RCPR_IMPORT_allocator_as(rcpr);
+RCPR_IMPORT_psock;
+
+/**
+ * \brief Cancel an assertion at the given offset, which will send a cancel
+ * response at that offset.
+ *
+ * \param sock      The socket on which this request is made.
+ * \param alloc     The allocator to use for this operation.
+ * \param offset    The unique offset for this assertion.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+status notificationservice_api_sendreq_assertion_cancel(
+    RCPR_SYM(psock)* sock, RCPR_SYM(allocator)* alloc, uint64_t offset)
+{
+    status retval, release_retval;
+    uint8_t* buf;
+    size_t bufsize;
+
+    /* parameter sanity checks. */
+    MODEL_ASSERT(prop_psock_valid(sock));
+    MODEL_ASSERT(rcpr_prop_allocator_valid(alloc));
+
+    /* runtime parameter checks. */
+    if (NULL == sock || NULL == alloc)
+    {
+        retval = AGENTD_ERROR_NOTIFICATIONSERVICE_API_BAD_ARGUMENT;
+        goto done;
+    }
+
+    /* encode request. */
+    retval =
+        notificationservice_api_encode_request(
+            &buf, &bufsize, alloc,
+            AGENTD_NOTIFICATIONSERVICE_API_METHOD_ID_BLOCK_UPDATE, offset,
+            NULL, 0U);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* send request. */
+    retval = psock_write_boxed_data(sock, buf, bufsize);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto cleanup_buffer;
+    }
+
+    /* success. */
+    retval = STATUS_SUCCESS;
+    goto cleanup_buffer;
+
+cleanup_buffer:
+    release_retval = rcpr_allocator_reclaim(alloc, buf);
+    if (STATUS_SUCCESS != release_retval)
+    {
+        retval = release_retval;
+    }
+
+done:
+    return retval;
+}
---
+++ test/notificationservice/test_notificationservice_api_sendreq_assertion_cancel.cpp
@@ -0,0 +1,85 @@
+/**
+ * \file
+ * test/notificatonservice/test_notificationservice_api_sendreq_assertion_cancel.cpp
+ *
+ * \brief Test notificationservice_api_sendreq_assertion_cancel.
+ *
+ * \copyright 2022 Velo-Payments, Inc.  All rights reserved.
+ */
+
+#include <agentd/bitcap.h>
+#include <agentd/inet.h>
+#include <agentd/notificationservice/api.h>
+#include <agentd/status_codes.h>
+#include <gtest/gtest.h>
+
+RCPR_IMPORT_allocator_as(rcpr);
+RCPR_IMPORT_psock;
+RCPR_IMPORT_resource;
+RCPR_IMPORT_uuid;
+
+/**
+ * \brief Test that the parameters are null checked.
+ */
+TEST(notificationservice_api_sendreq_assertion_cancel_test, argument_nullchecks)
+{
+    rcpr_allocator* alloc;
+    psock* sock;
+    uint64_t offset = 1234;
+
+    /* create an allocator instance. */
+    ASSERT_EQ(STATUS_SUCCESS, rcpr_malloc_allocator_create(&alloc));
+
+    /* create a dummy psock instance. */
+    ASSERT_EQ(
+        STATUS_SUCCESS,
+        psock_create_from_buffer(&sock, alloc, nullptr, 0));
+
+    /* if the socket is null, an error is returned. */
+    EXPECT_EQ(
+        AGENTD_ERROR_NOTIFICATIONSERVICE_API_BAD_ARGUMENT,
+        notificationservice_api_sendreq_assertion_cancel(
+            nullptr, alloc, offset));
+
+    /* if the allocator is null, an error is returned. */
+    EXPECT_EQ(
+        AGENTD_ERROR_NOTIFICATIONSERVICE_API_BAD_ARGUMENT,
+        notificationservice_api_sendreq_assertion_cancel(
+            sock, nullptr, offset));
+
+    /* clean up. */
+    ASSERT_EQ(STATUS_SUCCESS, resource_release(psock_resource_handle(sock)));
+    ASSERT_EQ(
+        STATUS_SUCCESS,
+        resource_release(rcpr_allocator_resource_handle(alloc)));
+}
+
+/**
+ * \brief Test that the request is sent.
+ */
+TEST(notificationservice_api_sendreq_assertion_cancel_test, basics)
+{
+    rcpr_allocator* alloc;
+    psock* sock;
+    uint64_t offset = 1234;
+
+    /* create an allocator instance. */
+    ASSERT_EQ(STATUS_SUCCESS, rcpr_malloc_allocator_create(&alloc));
+
+    /* create an output buffer psock instance. */
+    ASSERT_EQ(
+        STATUS_SUCCESS,
+        psock_create_from_buffer(&sock, alloc, nullptr, 0));
+
+    /* The request should succeed. */
+    EXPECT_EQ(
+        STATUS_SUCCESS,
+        notificationservice_api_sendreq_assertion_cancel(
+            sock, alloc, offset));
+
+    /* clean up. */
+    ASSERT_EQ(STATUS_SUCCESS, resource_release(psock_resource_handle(sock)));
+    ASSERT_EQ(
+        STATUS_SUCCESS,
+        resource_release(rcpr_allocator_resource_handle(alloc)));
+}
