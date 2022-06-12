d8ee054a52a3c7478d95f4847caa0bdcc692e8ba
Justin Handville <nanolith@gmail.com>
2022-06-12 04:41:13 -0400

    isolation test for assertion not matching block.

--- test/notificationservice/test_notificationservice_isolation.cpp
+++ test/notificationservice/test_notificationservice_isolation.cpp
@@ -268,7 +268,7 @@ TEST_F(notificationservice_isolation_test, block_update_not_authorized)
  * Test that we are immediately invalidated when the latest block has not been
  * set.
  */
-TEST_F(notificationservice_isolation_test, block_update_zero_block)
+TEST_F(notificationservice_isolation_test, block_assertion_zero_block)
 {
     uint8_t* buf = nullptr;
     size_t size = 0U;
@@ -281,6 +281,73 @@ TEST_F(notificationservice_isolation_test, block_update_zero_block)
     rcpr_uuid block_id = { .data = {
         0xdd, 0x4c, 0x97, 0x97, 0xcb, 0x8d, 0x4e, 0xaa,
         0xaa, 0x1f, 0x4e, 0xf9, 0x8c, 0x1e, 0x3a, 0xac } };
+
+    /* send block assertion request. */
+    ASSERT_EQ(
+        STATUS_SUCCESS,
+        notificationservice_api_sendreq_block_assertion(
+            client1, alloc, EXPECTED_OFFSET, &block_id));
+
+    /* get response. */
+    ASSERT_EQ(
+        STATUS_SUCCESS,
+        notificationservice_api_recvresp(
+            client1, alloc, &buf, &size));
+
+    /* decode response. */
+    ASSERT_EQ(
+        STATUS_SUCCESS,
+        notificationservice_api_decode_response(
+            buf, size, &method_id, &status_code, &offset, &payload,
+            &payload_size));
+
+    /* verify response. */
+    EXPECT_EQ(
+        AGENTD_NOTIFICATIONSERVICE_API_METHOD_ID_BLOCK_ASSERTION,
+        method_id);
+    EXPECT_EQ(0, status_code);
+    EXPECT_EQ(EXPECTED_OFFSET, offset);
+    EXPECT_EQ(nullptr, payload);
+
+    /* clean up. */
+    ASSERT_EQ(STATUS_SUCCESS, rcpr_allocator_reclaim(alloc, buf));
+}
+
+/**
+ * Test that a block assertion against a block other than the latest block
+ * update returns with an immediate invalidation.
+ */
+TEST_F(notificationservice_isolation_test, block_assertion_different_block)
+{
+    uint8_t* buf = nullptr;
+    size_t size = 0U;
+    const uint64_t EXPECTED_OFFSET = 7177;
+    uint32_t method_id;
+    uint32_t status_code;
+    uint64_t offset;
+    const uint8_t* payload = nullptr;
+    size_t payload_size = 0U;
+    rcpr_uuid latest_block_id = { .data = {
+        0xa4, 0xcf, 0x44, 0x00, 0x80, 0x0f, 0x48, 0x27,
+        0xba, 0xc3, 0x54, 0x2c, 0xfc, 0x56, 0xdf, 0x9d } };
+    rcpr_uuid block_id = { .data = {
+        0xdd, 0x4c, 0x97, 0x97, 0xcb, 0x8d, 0x4e, 0xaa,
+        0xaa, 0x1f, 0x4e, 0xf9, 0x8c, 0x1e, 0x3a, 0xac } };
+
+    /* send block update request. */
+    ASSERT_EQ(
+        STATUS_SUCCESS,
+        notificationservice_api_sendreq_block_update(
+            client1, alloc, EXPECTED_OFFSET, &latest_block_id));
+
+    /* get response. */
+    ASSERT_EQ(
+        STATUS_SUCCESS,
+        notificationservice_api_recvresp(
+            client1, alloc, &buf, &size));
+
+    /* reclaim buffer. */
+    ASSERT_EQ(STATUS_SUCCESS, rcpr_allocator_reclaim(alloc, buf));
 
     /* send block assertion request. */
     ASSERT_EQ(
