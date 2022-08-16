d86f88080aca20c1606445651ce8cea986c7c864
Justin Handville <nanolith@gmail.com>
2022-08-15 23:34:20 -0400

    Use simple parser.

--- src/canonization/canonizationservice_dataservice_response_block_read.c
+++ src/canonization/canonizationservice_dataservice_response_block_read.c
@@ -19,18 +19,6 @@
 static int parse_block(
     canonizationservice_instance_t* instance, const uint8_t* cert,
     size_t cert_size);
-static bool dummy_txn_resolver(
-    void* options, void* parser, const uint8_t* artifact_id,
-    const uint8_t* txn_id, vccrypt_buffer_t* output_buffer, bool* trusted);
-static int32_t dummy_artifact_state_resolver(
-    void* options, void* parser, const uint8_t* artifact_id,
-    vccrypt_buffer_t* txn_id);
-static int dummy_contract_resolver(
-    void* options, void* parser, const uint8_t* type_id,
-    const uint8_t* artifact_id, vccert_contract_closure_t* closure);
-static bool dummy_key_resolver(
-    void* options, void* parser, uint64_t height, const uint8_t* entity_id,
-    vccrypt_buffer_t* pubenckey_buffer, vccrypt_buffer_t* pubsignkey_buffer);
 
 /**
  * \brief Handle the response from the data service block read.
@@ -104,10 +92,8 @@ static int parse_block(
 
     /* initialize the parser options. */
     retval =
-        vccert_parser_options_init(
-            &parser_opts, &instance->alloc_opts, &instance->crypto_suite,
-            &dummy_txn_resolver, &dummy_artifact_state_resolver,
-            &dummy_contract_resolver, &dummy_key_resolver, NULL);
+        vccert_parser_options_simple_init(
+            &parser_opts, &instance->alloc_opts, &instance->crypto_suite);
     if (VCCERT_STATUS_SUCCESS != retval)
     {
         goto done;
@@ -154,49 +140,4 @@ cleanup_parser_opts:
 
 done:
     return retval;
-}
-
-/**
- * \brief Dummy transaction resolver.
- */
-static bool dummy_txn_resolver(
-    void* UNUSED(options), void* UNUSED(parser),
-    const uint8_t* UNUSED(artifact_id),
-    const uint8_t* UNUSED(txn_id), vccrypt_buffer_t* UNUSED(output_buffer),
-    bool* UNUSED(trusted))
-{
-    return false;
-}
-
-/**
- * \brief Dummy artifact state resolver.
- */
-static int32_t dummy_artifact_state_resolver(
-    void* UNUSED(options), void* UNUSED(parser),
-    const uint8_t* UNUSED(artifact_id), vccrypt_buffer_t* UNUSED(txn_id))
-{
-    return 0;
-}
-
-/**
- * \brief Dummy contract resolver.
- */
-static int dummy_contract_resolver(
-    void* UNUSED(options), void* UNUSED(parser), const uint8_t* UNUSED(type_id),
-    const uint8_t* UNUSED(artifact_id),
-    vccert_contract_closure_t* UNUSED(closure))
-{
-    return VCCERT_ERROR_PARSER_ATTEST_MISSING_CONTRACT;
-}
-
-/**
- * \brief Dummy key resolver.
- */
-static bool dummy_key_resolver(
-    void* UNUSED(options), void* UNUSED(parser), uint64_t UNUSED(height),
-    const uint8_t* UNUSED(entity_id),
-    vccrypt_buffer_t* UNUSED(pubenckey_buffer),
-    vccrypt_buffer_t* UNUSED(pubsignkey_buffer))
-{
-    return false;
 }
