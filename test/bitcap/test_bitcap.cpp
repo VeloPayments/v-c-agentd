e43afaf005c7e753ea5c661ffb8babf891ad506e
Justin Handville <nanolith@gmail.com>
2023-08-13 01:22:14 -0400

    Moved bitcap unit tests to minunit.

--- test/bitcap/test_bitcap.cpp
+++ test/bitcap/test_bitcap.cpp
@@ -3,22 +3,22 @@
  *
  * Test the bitcap system.
  *
- * \copyright 2018 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2018-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
 #include <agentd/bitcap.h>
+#include <minunit/minunit.h>
 #include <vpr/disposable.h>
 
-/* GTEST DISABLED */
-#if 0
-
 using namespace std;
+
+TEST_SUITE(bitcap_test);
 
 /**
  * Test that initializing the bitcap with all falses or all trues works as
  * expected.
  */
-TEST(bitcap_test, bitcap_init)
+TEST(bitcap_init)
 {
     BITCAP(b, 12);
 
@@ -26,41 +26,41 @@ TEST(bitcap_test, bitcap_init)
     BITCAP_INIT_FALSE(b);
 
     /* all bit values are false. */
-    EXPECT_FALSE(BITCAP_ISSET(b, 0));
-    EXPECT_FALSE(BITCAP_ISSET(b, 1));
-    EXPECT_FALSE(BITCAP_ISSET(b, 2));
-    EXPECT_FALSE(BITCAP_ISSET(b, 3));
-    EXPECT_FALSE(BITCAP_ISSET(b, 4));
-    EXPECT_FALSE(BITCAP_ISSET(b, 5));
-    EXPECT_FALSE(BITCAP_ISSET(b, 6));
-    EXPECT_FALSE(BITCAP_ISSET(b, 7));
-    EXPECT_FALSE(BITCAP_ISSET(b, 8));
-    EXPECT_FALSE(BITCAP_ISSET(b, 9));
-    EXPECT_FALSE(BITCAP_ISSET(b, 10));
-    EXPECT_FALSE(BITCAP_ISSET(b, 11));
+    TEST_EXPECT(!BITCAP_ISSET(b, 0));
+    TEST_EXPECT(!BITCAP_ISSET(b, 1));
+    TEST_EXPECT(!BITCAP_ISSET(b, 2));
+    TEST_EXPECT(!BITCAP_ISSET(b, 3));
+    TEST_EXPECT(!BITCAP_ISSET(b, 4));
+    TEST_EXPECT(!BITCAP_ISSET(b, 5));
+    TEST_EXPECT(!BITCAP_ISSET(b, 6));
+    TEST_EXPECT(!BITCAP_ISSET(b, 7));
+    TEST_EXPECT(!BITCAP_ISSET(b, 8));
+    TEST_EXPECT(!BITCAP_ISSET(b, 9));
+    TEST_EXPECT(!BITCAP_ISSET(b, 10));
+    TEST_EXPECT(!BITCAP_ISSET(b, 11));
 
     /* initialize the bitcap to all trues. */
     BITCAP_INIT_TRUE(b);
 
     /* all bit values are true. */
-    EXPECT_TRUE(BITCAP_ISSET(b, 0));
-    EXPECT_TRUE(BITCAP_ISSET(b, 1));
-    EXPECT_TRUE(BITCAP_ISSET(b, 2));
-    EXPECT_TRUE(BITCAP_ISSET(b, 3));
-    EXPECT_TRUE(BITCAP_ISSET(b, 4));
-    EXPECT_TRUE(BITCAP_ISSET(b, 5));
-    EXPECT_TRUE(BITCAP_ISSET(b, 6));
-    EXPECT_TRUE(BITCAP_ISSET(b, 7));
-    EXPECT_TRUE(BITCAP_ISSET(b, 8));
-    EXPECT_TRUE(BITCAP_ISSET(b, 9));
-    EXPECT_TRUE(BITCAP_ISSET(b, 10));
-    EXPECT_TRUE(BITCAP_ISSET(b, 11));
+    TEST_EXPECT(BITCAP_ISSET(b, 0));
+    TEST_EXPECT(BITCAP_ISSET(b, 1));
+    TEST_EXPECT(BITCAP_ISSET(b, 2));
+    TEST_EXPECT(BITCAP_ISSET(b, 3));
+    TEST_EXPECT(BITCAP_ISSET(b, 4));
+    TEST_EXPECT(BITCAP_ISSET(b, 5));
+    TEST_EXPECT(BITCAP_ISSET(b, 6));
+    TEST_EXPECT(BITCAP_ISSET(b, 7));
+    TEST_EXPECT(BITCAP_ISSET(b, 8));
+    TEST_EXPECT(BITCAP_ISSET(b, 9));
+    TEST_EXPECT(BITCAP_ISSET(b, 10));
+    TEST_EXPECT(BITCAP_ISSET(b, 11));
 }
 
 /**
  * Test that setting a bit to true or false works as expected.
  */
-TEST(bitcap_test, bitcap_set)
+TEST(bitcap_set)
 {
     BITCAP(b, 12);
 
@@ -68,25 +68,25 @@ TEST(bitcap_test, bitcap_set)
     BITCAP_INIT_FALSE(b);
 
     /* bit value 7 is false. */
-    ASSERT_FALSE(BITCAP_ISSET(b, 7));
+    TEST_ASSERT(!BITCAP_ISSET(b, 7));
 
     /* set bit value 7 to true. */
     BITCAP_SET_TRUE(b, 7);
 
     /* bit value 7 is true. */
-    ASSERT_TRUE(BITCAP_ISSET(b, 7));
+    TEST_ASSERT(BITCAP_ISSET(b, 7));
 
     /* set bit value 7 to false. */
     BITCAP_SET_FALSE(b, 7);
 
     /* bit value 7 is false. */
-    ASSERT_FALSE(BITCAP_ISSET(b, 7));
+    TEST_ASSERT(!BITCAP_ISSET(b, 7));
 }
 
 /**
  * Test that we can form an intersection of two bitcaps.
  */
-TEST(bitcap_test, bitcap_intersect)
+TEST(bitcap_intersect)
 {
     BITCAP(b, 12);
     BITCAP(c, 12);
@@ -108,15 +108,15 @@ TEST(bitcap_test, bitcap_intersect)
     BITCAP_INTERSECT(d, b, c);
 
     /* bit value 7 is true. */
-    ASSERT_TRUE(BITCAP_ISSET(d, 7));
+    TEST_ASSERT(BITCAP_ISSET(d, 7));
     /* bit value 8 is false. */
-    ASSERT_FALSE(BITCAP_ISSET(d, 8));
+    TEST_ASSERT(!BITCAP_ISSET(d, 8));
 }
 
 /**
  * Test that we can form a union of two bitcaps.
  */
-TEST(bitcap_test, bitcap_union)
+TEST(bitcap_union)
 {
     BITCAP(b, 12);
     BITCAP(c, 12);
@@ -138,10 +138,9 @@ TEST(bitcap_test, bitcap_union)
     BITCAP_UNION(d, b, c);
 
     /* bit value 6 is true. */
-    ASSERT_TRUE(BITCAP_ISSET(d, 6));
+    TEST_ASSERT(BITCAP_ISSET(d, 6));
     /* bit value 7 is true. */
-    ASSERT_TRUE(BITCAP_ISSET(d, 7));
+    TEST_ASSERT(BITCAP_ISSET(d, 7));
     /* bit value 8 is true. */
-    ASSERT_TRUE(BITCAP_ISSET(d, 8));
+    TEST_ASSERT(BITCAP_ISSET(d, 8));
 }
-#endif
