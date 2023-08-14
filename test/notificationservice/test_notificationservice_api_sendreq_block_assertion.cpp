26da7c8fbcfb55e5300ddcaec8985696a1289ec2
Justin Handville <nanolith@gmail.com>
2023-08-14 17:44:45 -0400

    Converted unit tests to minunit.

--- test/notificationservice/test_notificationservice_api_sendreq_block_assertion.cpp
+++ test/notificationservice/test_notificationservice_api_sendreq_block_assertion.cpp
@@ -4,26 +4,26 @@
  *
  * \brief Test notificationservice_api_sendreq_block_assertion.
  *
- * \copyright 2022 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2022-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
 #include <agentd/bitcap.h>
 #include <agentd/inet.h>
 #include <agentd/notificationservice/api.h>
 #include <agentd/status_codes.h>
-
-/* GTEST DISABLED */
-#if 0
+#include <minunit/minunit.h>
 
 RCPR_IMPORT_allocator_as(rcpr);
 RCPR_IMPORT_psock;
 RCPR_IMPORT_resource;
 RCPR_IMPORT_uuid;
 
+TEST_SUITE(notificationservice_api_sendreq_block_assertion_test);
+
 /**
  * \brief Test that the parameters are null checked.
  */
-TEST(notificationservice_api_sendreq_block_assertion_test, argument_nullchecks)
+TEST(argument_nullchecks)
 {
     rcpr_allocator* alloc;
     psock* sock;
@@ -33,42 +33,43 @@ TEST(notificationservice_api_sendreq_block_assertion_test, argument_nullchecks)
     uint64_t offset = 1234;
 
     /* create an allocator instance. */
-    ASSERT_EQ(STATUS_SUCCESS, rcpr_malloc_allocator_create(&alloc));
+    TEST_ASSERT(STATUS_SUCCESS == rcpr_malloc_allocator_create(&alloc));
 
     /* create a dummy psock instance. */
-    ASSERT_EQ(
-        STATUS_SUCCESS,
-        psock_create_from_buffer(&sock, alloc, nullptr, 0));
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == psock_create_from_buffer(&sock, alloc, nullptr, 0));
 
     /* if the socket is null, an error is returned. */
-    EXPECT_EQ(
-        AGENTD_ERROR_NOTIFICATIONSERVICE_API_BAD_ARGUMENT,
-        notificationservice_api_sendreq_block_assertion(
-            nullptr, alloc, offset, &block_id));
+    TEST_EXPECT(
+        AGENTD_ERROR_NOTIFICATIONSERVICE_API_BAD_ARGUMENT
+            == notificationservice_api_sendreq_block_assertion(
+                    nullptr, alloc, offset, &block_id));
 
     /* if the allocator is null, an error is returned. */
-    EXPECT_EQ(
-        AGENTD_ERROR_NOTIFICATIONSERVICE_API_BAD_ARGUMENT,
-        notificationservice_api_sendreq_block_assertion(
-            sock, nullptr, offset, &block_id));
+    TEST_EXPECT(
+        AGENTD_ERROR_NOTIFICATIONSERVICE_API_BAD_ARGUMENT
+            == notificationservice_api_sendreq_block_assertion(
+                    sock, nullptr, offset, &block_id));
 
     /* if the block id is null, an error is returned. */
-    EXPECT_EQ(
-        AGENTD_ERROR_NOTIFICATIONSERVICE_API_BAD_ARGUMENT,
-        notificationservice_api_sendreq_block_assertion(
-            sock, alloc, offset, nullptr));
+    TEST_EXPECT(
+        AGENTD_ERROR_NOTIFICATIONSERVICE_API_BAD_ARGUMENT
+            == notificationservice_api_sendreq_block_assertion(
+                    sock, alloc, offset, nullptr));
 
     /* clean up. */
-    ASSERT_EQ(STATUS_SUCCESS, resource_release(psock_resource_handle(sock)));
-    ASSERT_EQ(
-        STATUS_SUCCESS,
-        resource_release(rcpr_allocator_resource_handle(alloc)));
+    TEST_ASSERT(
+        STATUS_SUCCESS == resource_release(psock_resource_handle(sock)));
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == resource_release(rcpr_allocator_resource_handle(alloc)));
 }
 
 /**
  * \brief Test that the request is sent.
  */
-TEST(notificationservice_api_sendreq_block_assertion_test, basics)
+TEST(basics)
 {
     rcpr_allocator* alloc;
     psock* sock;
@@ -78,23 +79,23 @@ TEST(notificationservice_api_sendreq_block_assertion_test, basics)
     uint64_t offset = 1234;
 
     /* create an allocator instance. */
-    ASSERT_EQ(STATUS_SUCCESS, rcpr_malloc_allocator_create(&alloc));
+    TEST_ASSERT(STATUS_SUCCESS == rcpr_malloc_allocator_create(&alloc));
 
     /* create an output buffer psock instance. */
-    ASSERT_EQ(
-        STATUS_SUCCESS,
-        psock_create_from_buffer(&sock, alloc, nullptr, 0));
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == psock_create_from_buffer(&sock, alloc, nullptr, 0));
 
     /* The request should succeed. */
-    EXPECT_EQ(
-        STATUS_SUCCESS,
-        notificationservice_api_sendreq_block_assertion(
-            sock, alloc, offset, &block_id));
+    TEST_EXPECT(
+        STATUS_SUCCESS
+            == notificationservice_api_sendreq_block_assertion(
+                    sock, alloc, offset, &block_id));
 
     /* clean up. */
-    ASSERT_EQ(STATUS_SUCCESS, resource_release(psock_resource_handle(sock)));
-    ASSERT_EQ(
-        STATUS_SUCCESS,
-        resource_release(rcpr_allocator_resource_handle(alloc)));
+    TEST_ASSERT(
+        STATUS_SUCCESS == resource_release(psock_resource_handle(sock)));
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == resource_release(rcpr_allocator_resource_handle(alloc)));
 }
-#endif
