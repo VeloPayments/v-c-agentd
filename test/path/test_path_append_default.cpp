fb9a473fdf827bfd43845fbc6ab9ac03206c69ea
Justin Handville <nanolith@gmail.com>
2023-08-14 01:58:33 -0400

    Converted path append default tests to minunit.

--- test/path/test_path_append_default.cpp
+++ test/path/test_path_append_default.cpp
@@ -3,32 +3,32 @@
  *
  * Test the path_append_default method.
  *
- * \copyright 2018 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2018-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
 #include <agentd/commandline.h>
 #include <agentd/command.h>
 #include <agentd/path.h>
+#include <cstring>
+#include <minunit/minunit.h>
 #include <paths.h>
 #include <string>
 #include <vpr/disposable.h>
 
-/* GTEST DISABLED */
-
-#if 0
-
 using namespace std;
+
+TEST_SUITE(path_append_default);
 
 /**
  * \brief Append the default path onto an empty string.
  */
-TEST(path_append_default, empty_string)
+TEST(empty_string)
 {
     char* outpath = nullptr;
 
-    ASSERT_EQ(0, path_append_default("", &outpath));
+    TEST_ASSERT(0 == path_append_default("", &outpath));
 
-    EXPECT_STREQ(_PATH_DEFPATH, outpath);
+    TEST_EXPECT(!strcmp(_PATH_DEFPATH, outpath));
 
     free(outpath);
 }
@@ -36,14 +36,14 @@ TEST(path_append_default, empty_string)
 /**
  * \brief Append the default path onto an arbitrary path
  */
-TEST(path_append_default, arbitrary_path_1)
+TEST(arbitrary_path_1)
 {
     char* outpath = nullptr;
     string arbitrary_path = string("baz") + string(":") + _PATH_DEFPATH;
 
-    ASSERT_EQ(0, path_append_default("baz", &outpath));
+    TEST_ASSERT(0 == path_append_default("baz", &outpath));
 
-    EXPECT_STREQ(arbitrary_path.c_str(), outpath);
+    TEST_EXPECT(!strcmp(arbitrary_path.c_str(), outpath));
 
     free(outpath);
 }
@@ -51,17 +51,16 @@ TEST(path_append_default, arbitrary_path_1)
 /**
  * \brief Append the default path onto an arbitrary path
  */
-TEST(path_append_default, arbitrary_path_2)
+TEST(arbitrary_path_2)
 {
     char* outpath = nullptr;
     string begin_path("/bin:/usr/bin:/home/foo/bin");
     string arbitrary_path =
         string("/bin:/usr/bin:/home/foo/bin") + string(":") + _PATH_DEFPATH;
 
-    ASSERT_EQ(0, path_append_default(begin_path.c_str(), &outpath));
+    TEST_ASSERT(0 == path_append_default(begin_path.c_str(), &outpath));
 
-    EXPECT_STREQ(arbitrary_path.c_str(), outpath);
+    TEST_EXPECT(!strcmp(arbitrary_path.c_str(), outpath));
 
     free(outpath);
 }
-#endif
